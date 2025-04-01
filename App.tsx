e9e6a56dc7a7227246379bb72eda0ee88e59d2a3
idrissa183 <ouedraogoidrissa7108@gmail.com>
2025-04-01 19:22:07 +0000

    Configuration de tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './global.css';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View className="flex-1 justify-center items-center bg-white" >
+            <Text className="text-3xl text-red-400 font-bold">Hello, World</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
